b320330e888ec64ab494e7c334585b667cf3499b
NurkaAmre <nurkerey30@gmail.com>
2023-04-21 15:52:59 +0600

    Add card icon and number of items +style

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,15 +6,22 @@ import Image from "next/image"
 import Link from "next/link"
 import Cart from "./Cart"
 import { userCartStore } from "@/store"
+import {AiFillShopping} from 'react-icons/ai'
 
 const Nav = ({user}: Session) => {
   const cartStore = userCartStore()
   return (
-    <nav className="flex justify-between items-center py-8">
+    <nav className="flex justify-between items-center py-12">
       <Link href={'/'}>
         <h1>Styled</h1>
       </Link> 
       <ul className="flex items-center gap-12">
+        <li className="flex items-center text-3xl relative cursor-pointer">
+          <AiFillShopping />
+          <span className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center">
+            {cartStore.cart.length}
+          </span>
+        </li>
         {!user && (
           <li className="bg-teal-400 text-white py-2 px-4 rounded-md">
             <button onClick={() => signIn()}>SignIn</button>
