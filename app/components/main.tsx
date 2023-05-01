54913805510e0e7b492f15965d4d50638ef56d74
Amine <ouas.amine@outlook.com>
2023-05-01 16:51:21 +0100

    Replace image tags with bg-img css property in main page

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -7,8 +7,7 @@ import Image from 'next/image'
 export default function MainSection (){
   return (
     <>
-    <div className='main min-h-screen flex justify-center flex-col align-middle text-center'>
-    <Image src={pattern} alt="pattern" className='pattern'/>
+    <div className="main min-h-screen flex justify-center flex-col align-middle text-center bg-[url('../public/pattern_left.png'),_url('../public/pattern_right.png')] bg-[position:top_left,_bottom_right] bg-no-repeat">
     <hr className="h-4 hr bg-gradient-to-r from-gray-300 to-transparent border-transparent w-2/3"/>
     <div className='flex justify-center items-center text-center'>
       <h1 className='font-bold text-2xl heading '>Modern</h1>
