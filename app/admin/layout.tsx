6b648e15774cdb6b92ea9538f83ba7a30a8774d6
Amine <ouas.amine@outlook.com>
2023-05-13 15:53:13 +0100

    Create admin nav bar component

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,9 @@
+import Nav from "../components/AdminNav";
+
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <>
-      <h1>NavBar</h1>
+      <Nav />
       {children}
     </>
   )
