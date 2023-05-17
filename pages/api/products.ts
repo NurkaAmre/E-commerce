a9509878b5783098592ff5878bd5963ac963da7e
Amine <ouas.amine@outlook.com>
2023-05-17 15:02:46 +0100

    Insert product details into database

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,9 +1,30 @@
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const body = req.body;
+  const prisma = new PrismaClient();
  
-  console.log('body: ', body);
- 
-  res.status(200).json({ response: 'ok'});
+  const createProduct = prisma.product.create({
+    data: {
+      name: body.name,
+      description: body.description,
+      price: body.price,
+      image: body.image,
+      colours: body.colours,
+      category: body.category,
+      set: body.set,
+    }
+  })
+  createProduct
+    .then((data) => {
+      res.status(200).json({ response: 'success', data: data})
+    })
+    .catch((err) => {
+      console.log('Error Message:', err);
+      
+      res.status(400).json('Something went wrong')
+    });
+
+  
 }
