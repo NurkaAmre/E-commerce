9863638bc375531fab40ec6969958f5d83c5d6f6
Amine <ouas.amine@outlook.com>
2023-05-23 18:17:18 +0100

    Display Sets images & names on Sets index page

--- app/sets/page.tsx
+++ app/sets/page.tsx
@@ -1,4 +1,6 @@
 import { createClient } from "next-sanity";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Sets () {
   const client = createClient({
@@ -6,12 +8,23 @@ export default async function Sets () {
     dataset: "production",
     useCdn: true,
   })
-  const query = `*[_type == "set"]`
+  const query = `*[_type == "set"]
+                {_id, name, slug, price, description, "imageUrl": image.asset->url}`
   const sets = await client.fetch(query);
   return (
       <div>
         {/* {console.log(sets)} */}
           <h1>Sets</h1>
+          <div className="flex flex-wrap">
+            {sets.map((set: any) => (
+              <Link href={`/sets/${set.slug.current}`} key={set._id}>
+                <div className="p-4">
+                  <h2>{set.name}</h2>
+                  <Image src={set.imageUrl} alt={set.name} width={200} height={200} />
+                </div>
+              </Link>
+            ))}
+          </div>
       </div>
   )
 }
