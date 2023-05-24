762202eca0501448b7ac777a00d657a37448e189
Amine <ouas.amine@outlook.com>
2023-05-24 11:44:10 +0100

    Fetch and display Set & its products details on Set details route

--- app/sets/[slug]/page.tsx
+++ app/sets/[slug]/page.tsx
@@ -1,7 +1,30 @@
+import SanityClient from "@/sanity/client";
+import Image from "next/image";
+
 export default async function Set ({params}: {params: {slug: string}}) {
+  const SetSlug = params.slug;
+  const query = `*[_type == "set" && slug.current == '${SetSlug}']
+                {name, slug, price, description, "SetImageUrl": image.asset->url,
+                'products': products[]->{name, price, details, slug, quantity, "ProductImagesUrls": images[].asset->url}}`
+  const queryResult = await SanityClient.fetch(query);
+  const SetDetails = queryResult[0];
   return (
-      <div>
-          <h1>Set Details of ({params.slug})</h1>
+      <div className="w-96 mx-auto flex flex-col items-center">
+        <h1>Set Details of ({SetDetails.name})</h1>
+        <h3>Price: {SetDetails.price}</h3>
+        <Image src={SetDetails.SetImageUrl} alt={SetDetails.name} width={200} height={200} />
+        <p>{SetDetails.description}</p>
+        <h2>Products:</h2>
+        <div className="flex">
+          {SetDetails.products.map((product) => (
+            <div className="flex flex-col p-2 border border-red-500">
+              <h2>Product: {product.name}</h2>
+              <h3>Price: {product.price}</h3>
+              <Image src={product.ProductImagesUrls[0]} alt={product.name} width={200} height={200} />
+              <p>{product.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
   )
 }
