b0969c6abf146349d078ba7421c58802e91d49db
Amine <ouas.amine@outlook.com>
2023-06-03 20:45:04 +0100

    Create first ver of mobile menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react"
 import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
-import { AiFillShopping, AiFillHeart, AiOutlineMenu } from 'react-icons/ai'
+import { AiFillShopping, AiFillHeart, AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { FiPhoneCall } from 'react-icons/fi';
 import { motion, AnimatePresence } from "framer-motion"
 import { userCartStore } from "@/store"
@@ -16,11 +16,16 @@ import logo from '@/public/logo2.png'
 
 const Nav = ({ user }:  any) => {
   const cartStore = userCartStore()
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+  const [showCategoriesMenu, setShowCategoriesMenu] = useState(false)
   const menuButtonClick = () => {
-    setShowMenu(!showMenu)
+    setShowMobileMenu(!showMobileMenu)
+  }
+  const categoriesButtonClick = () => {
+    setShowCategoriesMenu(!showCategoriesMenu)
   }
   return (
+    <>
     <nav className="flex fixed justify-between text-gray-600 items-center gap-4 px-10 py-4 font-castoro w-full z-20">
       <button className="md:hidden text-3xl" onClick={menuButtonClick}><AiOutlineMenu></AiOutlineMenu></button>
       <Link href={'/'} className="hidden md:block">
@@ -78,8 +83,37 @@ const Nav = ({ user }:  any) => {
           </li>
         )}
       </ul>
-
     </nav>
+
+    {showMobileMenu && (
+      <nav className="absolute top-0 bottom-0 right-0 left-0 bg-gray-400 z-50">
+      <button className="absolute right-1 top-1 text-3xl" onClick={menuButtonClick}>
+        <AiOutlineClose></AiOutlineClose>
+      </button>
+      <Link href={'/'} className="block">
+        <Image src={logo} width={70} height={70} alt="logo" />
+      </Link>
+      <ul className="flex flex-col">
+        <li className="mr-6 cursor-pointer whitespace-nowrap" onClick={categoriesButtonClick}>
+        <Link href={'#'} onClick={categoriesButtonClick}>Товары</Link>
+          {showCategoriesMenu && (
+            <ul className="hidden absolute group-hover:flex flex-col gap-1 bg-gray-400 z-40 border-2 border-red-500">
+              <li><Link href={'/category/chairs'} className="hover:text-white">Chairs</Link></li>
+              <li><Link href={'/category/kitchens'} className="hover:text-white">Kitchens</Link></li>
+              <li><Link href={'/category/sofas'} className="hover:text-white">Sofas</Link></li>
+          </ul>
+          )}
+        </li>
+        <li className="mr-6 cursor-pointer whitespace-nowrap">
+          <Link href={'/aboutus'}>О Компании</Link>
+        </li>
+        <li className="mr-6 cursor-pointer whitespace-nowrap">
+          <Link href={'/sale'}>Акции</Link>
+        </li>
+      </ul>
+    </nav>
+    )}
+    </>
   )
 }
 
