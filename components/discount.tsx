499892bb80ebb3a5f6e7354b4ae6650b52484d5e
Amine <ouas.amine@outlook.com>
2023-06-03 13:34:02 +0100

    Display fetched products images

--- components/discount.tsx
+++ components/discount.tsx
@@ -1,13 +1,20 @@
-import bed from '@/public/spalniy_gar1.jpeg'
-import bed1 from '@/public/spalniy_gar7.jpeg'
-import bed2 from '@/public/spalniy_gar4.jpeg'
 import Image from 'next/image'
 import SanityClient from '@/sanity/client';
 
 export default async function Discount (){
-  const query = `*[_type == "product" && defined(discount)][0...20]`;
+  const query = `*[_type == "product" && defined(discount)]
+                {
+                  _id,
+                  name,
+                  price,
+                  quantity,
+                  details,
+                  type,
+                  description,
+                  discount,
+                  "imagesURL": images[].asset->url
+                }[0...20]`;
   const products = await SanityClient.fetch(query);
-  console.log(products);
   
   return (
     <> 
@@ -25,9 +32,10 @@ export default async function Discount (){
         </div> */}
         <div className=" marquee">
           <div className='justify-center rounded-img relative ml-10 maylike-products-container track flex flex-col discount-img'>
-            <div className="img2 image-container items-center rounded-full "><Image className=' rounded-full' src={bed} alt='bed' /></div>
-            <div className="img1 image-container items-center rounded-full "><Image className=' rounded-full' src={bed1} alt='bed' /></div>
-            <div className="img3 image-container items-center rounded-full "><Image className=' rounded-full' src={bed2} alt='bed' /></div>
+            {products.map((product) => (
+              <Image className='border-2 border-red-500 m-4' src={product.imagesURL[1]} width={200} height={200} alt='bed' />
+            ))}
+
           </div>
         </div>
 
