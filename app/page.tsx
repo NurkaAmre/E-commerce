36d5157d5c1426586ff78b7a0920de0b5bf76f47
NurkaAmre <nurkerey30@gmail.com>
2023-06-03 16:01:54 +0600

    About Us Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import MainSection from "../components/main"
 import ProductSection from "../components/product-section"
 import DeliveryServices from "../components/DeliveryServices"
 
+
 export default function Main (){
   return (
     <>
