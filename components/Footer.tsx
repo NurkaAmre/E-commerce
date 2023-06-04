eb74eb50bfe012ed4f086197603be0291a667fdb
Amine <ouas.amine@outlook.com>
2023-06-04 14:33:38 +0100

    Center text in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,21 +8,21 @@ function Footer() {
   return (
     <footer>
       <div className="text-white flex flex-col md:flex-row justify-evenly font-castoro items-center md:items-start py-20">
-        <div className="text-white py-4">
+        <div className="text-white py-4 text-center">
           <h2 className='font-bold'>Режим работы call-центра</h2>
           <p>ежедневно с 11:00 до 00:00</p>
           <h2 className='font-bold'>Доставка заказов</h2>
           <p>ежедневно с 9:00 до 23:00</p>
         </div>
        
-        <div className="text-white py-4">
+        <div className="text-white py-4 text-center">
           <h2 className='font-bold'>Контактная информация</h2>
           <p>тел: +7707-717-91-28</p>
           <p>Алматы, Аккент 56-66</p>
           <p>info@furniturewebsite.com</p>
         </div>
 
-        <div className='py-4'>
+        <div className='py-4 text-center'>
           <h2 className='font-bold'>Follow Us:</h2>
           <ul className="flex justify-evenly gap-2">
             <Link href="https://www.facebook.com/" className="hover:text-gray-200 text-3xl"><AiFillFacebook/></Link>
