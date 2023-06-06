88831a6b273f9094d08e198f3884dc77f314aba7
Amine <ouas.amine@outlook.com>
2023-06-06 13:41:10 +0100

    Display user menu upon user image hover instead of click

--- components/UserOptions.tsx
+++ components/UserOptions.tsx
@@ -4,12 +4,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const UserOptions = ({ user }: any) => {
-  const [optionsState, setOptionsState] = useState(false)
     return (
-      <>
+      <div className='group'>
         <Image src={user.image} width={50} height={50} onClick={() => { setOptionsState(!optionsState) }} alt="user" className="rounded-full w-full"/>
-        {optionsState && (
-          <div className="flex flex-col items-center hidden-user pl-[0.5rem] pr-[0.5rem] pt-[0.5rem] pb-[0.5rem] absolute top-25 right-2">
+          <div className="flex-col items-center hidden hidden-user group-hover:flex pl-[0.5rem] pr-[0.5rem] pt-[0.5rem] pb-[0.5rem] absolute top-25 right-2">
             <span className="font-bold text-gray-500">{user.name}</span>
             <hr className='bg-white' />
             <Link href="/user-info" className="w-full block hidden-text text-center">Личные данные</Link>
@@ -18,8 +16,7 @@ const UserOptions = ({ user }: any) => {
             <hr />
             <button onClick={() => { signOut() }} className="w-full hidden-text block text-center">Выйти</button>
         </div>
-        )}
-      </>
+      </div>
     );
 }
 
