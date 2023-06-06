0fb66369b257cfa2f3c857d33d30b9a49b36e83e
Amine <ouas.amine@outlook.com>
2023-06-06 16:47:43 +0100

    Implement first ver of favlist & extract product component on cat page

--- components/FavList.tsx
+++ components/FavList.tsx
@@ -3,15 +3,20 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function FavList ({ userFavList }: { userFavList: ProductType[]}) {
-  console.log(userFavList);
-  
   return (
-    <div className="flex flex-col items-center justify-center absolute">
-      <Link href={'product url'}>
-        <Image src={'/public/sofa.png'} width={70} height={70} alt={'Product'} />
-        <h3>{'Product Name'}</h3>
-        <p>{'Product Price'}</p>
-      </Link>
+    <div className="flex flex-col items-center justify-center absolute right-10 bg-white border-2 border-red-500 min-h-[200px] min-w-[300px]">
+      {
+        userFavList.map((product: ProductType) => (
+          <Link 
+            href={`/product/${product.slug.current}`}
+            className='flex items-center justify-center gap-4 border-2 border-gray-400'
+          >
+            <Image src={product.imagesURL[0]} width={70} height={70} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+          </Link>
+        ))
+      }
     </div>
   )
 }
