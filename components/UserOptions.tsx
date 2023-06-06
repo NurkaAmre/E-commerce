fbfc81deafa9fc5807341906e28fe4721fc917a6
Amine <ouas.amine@outlook.com>
2023-06-06 13:51:06 +0100

    Delete un-used function in userOption component

--- components/UserOptions.tsx
+++ components/UserOptions.tsx
@@ -1,13 +1,12 @@
 import { signOut } from 'next-auth/react';
-import { useState } from 'react'; 
 import Image from 'next/image';
 import Link from 'next/link';
 
 const UserOptions = ({ user }: any) => {
     return (
       <div className='group'>
-        <Image src={user.image} width={50} height={50} onClick={() => { setOptionsState(!optionsState) }} alt="user" className="rounded-full w-full"/>
-          <div className="flex-col items-center hidden hidden-user group-hover:flex pl-[0.5rem] pr-[0.5rem] pt-[0.5rem] pb-[0.5rem] absolute top-25 right-2">
+        <Image src={user.image} width={50} height={50} alt="user image" className="rounded-full w-full"/>
+          <div className="flex-col items-center hidden hidden-user group-hover:flex p-[0.5rem] absolute top-25 right-0 z-40">
             <span className="font-bold text-gray-500">{user.name}</span>
             <hr className='bg-white' />
             <Link href="/user-info" className="w-full block hidden-text text-center">Личные данные</Link>
