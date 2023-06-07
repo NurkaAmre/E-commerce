cae98d1bf0b3c41820552897686e958bfc9d35cf
NurkaAmre <nurkerey30@gmail.com>
2023-06-07 10:04:20 +0600

    Change discount bg

--- components/discount.tsx
+++ components/discount.tsx
@@ -28,10 +28,10 @@ export default async function Discount (){
                 <div className='flex flex-col'>
 
                   <div className='relative'>
-                    <div className="discount absolute right-4 top-4">
-                      <p className="pl-1">-{product.discount}%</p>
+                    <div className="discount1 absolute right-4 top-4">
+                      <p className="pl-1 pt-2">-{product.discount}%</p>
                     </div>
-                    <Image className=' m-2 rounded-md' src={product.imagesURL[1]} width={200} height={200} alt='bed' />
+                    <Image className=' m-2 rounded-md' src={product.imagesURL[1]} width={200} height={400} alt='bed' />
                     <div className='flex flex-row bottom-[2rem] absolute bg-slate-500 bg-opacity-50 w-full rounded-md gap-2 py-3 justify-center'>
                       <h1 className='text-start ml-4 text-xl text-white'>{product.name}</h1>
                       <h3 className='text-white'>{product.price}&#x20B8;</h3>
