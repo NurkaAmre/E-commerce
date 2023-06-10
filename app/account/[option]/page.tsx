7f751084980e735271f6eaaa717626c6747bea35
Amine <ouas.amine@outlook.com>
2023-06-10 17:03:33 +0100

    Check if user is signed before displaying user settings

--- app/account/[option]/page.tsx
+++ app/account/[option]/page.tsx
@@ -1,7 +1,17 @@
 import FavList from '@/components/FavList'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 
-export default function Account({ params: { option } }: { params: { option: string } }) {
+export default async function Account({ params: { option } }: { params: { option: string } }) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) 
+    return (
+      <main className="mt-[100px] flex">
+        <h1>You need to login to access this page</h1>
+      </main>
+    )
+  
   return (
     <main className="mb-20 flex">
       <nav className='pt-[100px] px-6'>
