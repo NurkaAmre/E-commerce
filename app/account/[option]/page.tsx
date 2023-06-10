a800faf398dbc6bfd4471d8cfca744f710cf5db7
Amine <ouas.amine@outlook.com>
2023-06-10 18:38:37 +0100

    Use Product component to diplay favlist items

--- app/account/[option]/page.tsx
+++ app/account/[option]/page.tsx
@@ -7,21 +7,21 @@ export default async function Account({ params: { option } }: { params: { option
   const session = await getServerSession(authOptions);
   if (!session?.user) 
     return (
-      <main className="mt-[100px] flex">
+      <main className="mt-[100px] flex min-h-[50vh]">
         <h1>You need to login to access this page</h1>
       </main>
     )
   
   return (
-    <main className="mb-20 flex">
-      <nav className='pt-[100px] px-6'>
+    <main className="flex min-h-[60vh]">
+      <nav className='pt-[100px] px-2'>
         <ul>
-          <li><Link href={"/account/info"}>Info</Link></li>
-          <li><Link href={"/account/orders"}>Orders</Link></li>
-          <li><Link href={"/account/wishlist"}>Wish List</Link></li>
+          <li><Link href={"/account/info"}>My Info</Link></li>
+          <li><Link href={"/account/orders"}>My Orders</Link></li>
+          <li><Link href={"/account/wishlist"}>WishList</Link></li>
         </ul>
       </nav>
-      <section className='pt-[100px]'>
+      <section className='flex flex-col items-center w-full pt-[100px] pb-10 px-4'>
         {option === 'wishlist' && <FavList />}
       </section>
     </main>
--- components/FavList.tsx
+++ components/FavList.tsx
@@ -4,23 +4,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { ProductType } from '@/types/ProductType'
 import { userFavStore } from "@/store"
+import Product from './Product'
 
 
 export default function FavList () {
   const favList = userFavStore((state) => state.favList)
 
   return (
-    <div className="flex flex-col items-center gap-4 justify-center bg-white border-2 border-red-500 min-h-[200px] min-w-[300px]">
+    <div className="flex flex-wrap w-full items-center gap-4 justify-center">
       {
         favList.map((product: ProductType) => (
-          <Link 
-            href={`/product/${product.slug.current}`}
-            className='flex items-center justify-center gap-4 border-2 border-gray-400'
-          >
-            <Image src={product.imagesURL[0]} width={200} height={200} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-          </Link>
+          <Product product={product} />
         ))
       }
     </div>
