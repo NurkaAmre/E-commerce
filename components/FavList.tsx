7d7c3dbf2cc74880961d3e1dfbf5cef0a477d088
Amine <ouas.amine@outlook.com>
2023-06-11 14:15:52 +0100

    Fix wishlist header placement

--- components/FavList.tsx
+++ components/FavList.tsx
@@ -11,13 +11,15 @@ export default function FavList () {
   const favList = userFavStore((state) => state.favList)
 
   return (
-    <div className="flex flex-wrap w-full items-center gap-4 justify-center">
+    <div className="flex flex-col w-full items-center gap-4 justify-center">
       <h2>A list of products you liked</h2>
-      {
-        favList.map((product: ProductType) => (
-          <Product product={product} />
-        ))
-      }
+      <div className="flex flex-wrap w-full items-center gap-4 justify-center">
+        {
+          favList.map((product: ProductType) => (
+            <Product product={product} />
+          ))
+        }
+      </div>
     </div>
   )
 }
