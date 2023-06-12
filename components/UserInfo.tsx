7a8b190743163834a493404dcfc47813de4ec2e2
NurkaAmre <nurkerey30@gmail.com>
2023-06-13 00:05:19 +0600

    Style users info page

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import { AiFillEdit } from 'react-icons/ai'
 
 export default function UserInfo({ user }: any) {
   // Edit mode status for each card
@@ -8,6 +9,8 @@ export default function UserInfo({ user }: any) {
   const [addressEditMode, setaddressEditMode] = useState(false)
   // User info data
   const [name, setName] = useState(user.name)
+  const [surname, setSurname] = useState(user.surname)
+  const [familyname, setFamilyname] = useState(user.familyname)
   const [email, setEmail] = useState(user.email)
   const [phone, setPhone] = useState(user.phone)
   // User address data
@@ -18,6 +21,12 @@ export default function UserInfo({ user }: any) {
   // Change event handlers for user info
   const handleNameChange = (e: React.FormEvent<HTMLInputElement>) => {
     setName(e.currentTarget.value)
+  }
+  const handleSurnameChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setSurname(e.currentTarget.value)
+  }
+  const handleFamilynameChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setFamilyname(e.currentTarget.value)
   }
   const handleEmailChange = (e: React.FormEvent<HTMLInputElement>) => {
     setEmail(e.currentTarget.value)
@@ -50,58 +59,64 @@ export default function UserInfo({ user }: any) {
   }
 
   return (
-    <div className="flex flex-wrap w-full items-center gap-4 justify-center">
-      {/* Info card */}
-      <div className='flex flex-col bg-slate-400/25 p-4 w-[300px] rounded-lg'>
-        <h3 className='text-center'>My Info</h3>
-        <div>
-          <span>Full Name: </span>
-          {!infoEditMode ? <span>{user.name}</span> 
-            : <input type="text" value={name} onChange={handleNameChange} />}
+    <section className="flex flex-col items-center justify-center">
+      <div className='flex flex-col md:flex-row md:gap-[6rem]'>
+        <div className='flex flex-col  relative'>
+          <AiFillEdit className='absolute text-[#8CCCC1] right-0 top-5 text-2xl' onClick={() => setUserInfoEditMode(true)} />
+          <h3 className='text-2xl font-castoro font-bold text-left  text-gray-600 my-4'>Личные данные</h3>
+          <div className='flex flex-col my-3 text-gray-700'>
+            <label className='text-sm'>Имя<span className='text-red-600'>*</span> </label>
+            <input className='user-input' type="text" value={name} onChange={handleNameChange} />
+          </div>
+          <div className='flex flex-col my-3'>
+            <label className='text-sm'>Фамилия<span className='text-red-600'>*</span> </label>
+            {!infoEditMode ? <span className='user-input'>{user.surname}</span>
+              : <input className='user-input' type="text" value={surname} onChange={handleSurnameChange} />}
+          </div>
+          <div className='flex flex-col my-3'>
+            <label className='text-sm'>Отчество</label>
+            {!infoEditMode ? <span className='user-input'>{user.familyname}</span>
+              : <input className='user-input' type="text" value={familyname} onChange={handleFamilynameChange} />}
+          </div>
         </div>
-        <div>
-          <span>E-mail: </span>
-          {!infoEditMode ? <span>{user.email}</span>
-            : <input type="email" value={email} onChange={handleEmailChange} />}
+        <div className='flex flex-col relative'>
+          <h3 className='text-2xl my-4 font-castoro font-bold text-gray-600'>Контакты</h3>
+          <AiFillEdit className='absolute  text-[#8CCCC1]  right-0 top-5 text-2xl' onClick={() => setUserInfoEditMode(true)} />
+          <div className='flex flex-col my-3  text-gray-700'>
+            <label className='text-sm'>Тел<span className='text-red-600'>*</span> </label>
+            {!infoEditMode ? <span className='user-input'>{user.phone}</span>
+              : <input className='user-input' type="tel" value={phone} onChange={handlePhoneChange} />}
+          </div>
+          <div className='flex flex-col my-3'>
+            <label className='text-sm'>E-mail<span className='text-red-600'>*</span> </label>
+            {!infoEditMode ? <span className='user-input'>{user.email}</span>
+              : <input className='user-input' type="email" value={email} onChange={handleEmailChange} />}
+          </div>
         </div>
-        <div>
-          <span>Phone: </span>
-          {!infoEditMode ? <span>{user.phone}</span>
-            : <input type="tel" value={phone} onChange={handlePhoneChange} />}
-        </div>
-        <div>
-          <button onClick={() => setUserInfoEditMode(true)} className='bg-slate-400/25 px-4 py-2 rounded-lg'>Edit</button>
-          <button onClick={() => updateUserInfo()} className='bg-slate-400/25 px-4 py-2 rounded-lg'>Save</button>
+        <div className='flex flex-col relative '>
+          <h2 className='text-2xl my-4 font-castoro font-bold text-gray-600 '>Адрес</h2>
+          <AiFillEdit className='absolute  text-[#8CCCC1]  right-0 top-5 text-2xl' onClick={() => setUserInfoEditMode(true)} />
+          <div className='flex flex-col my-3 text-gray-700'>
+            <label className='text-sm'>Город<span className='text-red-600'>*</span> </label>
+            {!addressEditMode ? <span className='user-input'>{user.address?.city}</span>
+              : <input className='user-input' type="text" value={city} onChange={handleCityChange} />}
+          </div>
+          <div className='flex flex-col my-3'>
+            <label className='text-sm'>Адрес доставки<span className='text-red-600'>*</span> </label>
+            {!addressEditMode ? <span className='user-input'>{user.address?.address}</span>
+              : <input className='user-input' type="text" value={address} onChange={handleAddressChange} />}
+          </div>
+          <div className='flex flex-col my-3'>
+            <label className='text-sm'>Индекс<span className='text-red-600'>*</span> </label>
+            {!addressEditMode ? <span className='user-input'>{user.address?.zip}</span>
+              : <input className='user-input' type="text" value={zip} onChange={handleZipChange} />}
+          </div>
+
         </div>
       </div>
-      {/* Address card */}
-      <div className='flex flex-col bg-slate-400/25 p-4 w-[300px] rounded-lg'>
-        <h3 className='text-center'>My Address</h3>
-        <div>
-        <span>Address: </span>
-        {!addressEditMode ? <span>{user.address?.address}</span> 
-            : <input type="text" value={address} onChange={handleAddressChange} />}
-        </div>
-        <div>
-          <span>City: </span>
-          {!addressEditMode ? <span>{user.address?.city}</span> 
-            : <input type="text" value={city} onChange={handleCityChange} />}
-        </div>
-        <div>
-          <span>Province: </span>
-          {!addressEditMode ? <span>{user.address?.province}</span> 
-            : <input type="text" value={province} onChange={handleProvinceChange} />}
-        </div>
-        <div>
-          <span>ZIP Code: </span>
-          {!addressEditMode ? <span>{user.address?.zip}</span> 
-            : <input type="text" value={zip} onChange={handleZipChange} />}
-        </div>
-        <div>  
-          <button onClick={() => setaddressEditMode(true)} className='bg-slate-400/25 px-4 py-2 rounded-lg'>Edit</button>
-          <button onClick={() => updateUserAddress()} className='bg-slate-400/25 px-4 py-2 rounded-lg'>Save</button>
-        </div>
+      <div className='my-3'>
+        <button onClick={() => updateUserInfo()} className='btn min-w-full rounded-full'>Сохранить</button>
       </div>
-    </div>
+    </section >
   )
 }
