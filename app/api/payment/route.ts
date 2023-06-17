c42c93799504ae93fd7411259a0fa7b2314a9b4f
Amine <ouas.amine@outlook.com>
2023-06-17 18:51:41 +0100

    Fix test object

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -5,7 +5,7 @@ import xml2js from 'xml2js'
 export async function POST(request: Request) {
   const response = await request.text()
   const parser = new xml2js.Parser()
-  const { test } = await parser.parseStringPromise(response)
+  const test = await parser.parseStringPromise(response)
   await SanityClient.patch('00675708-865c-4187-88dd-de3bce751590').set({
     title: `${test.pg_result}`,
   }).commit()
