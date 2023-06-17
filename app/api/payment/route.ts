365057dffe9b3325d10a1650ade3c5874da4b729
Amine <ouas.amine@outlook.com>
2023-06-17 18:42:45 +0100

    Extract Request.text() promise

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -2,10 +2,9 @@ import SanityClient from "@/sanity/client"
 import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
+  const response = await request.text()
   await SanityClient.patch('00675708-865c-4187-88dd-de3bce751590').set({
-    title: `${request.text()}`,
+    title: `${response}`,
   }).commit()
-  .then((res: any) => res)
-  .catch((err: any) => err)
   return NextResponse.json({ message: 'Hello world!' })
 }
