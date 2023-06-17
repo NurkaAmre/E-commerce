0115353e0bbcc2a75b1667f952db8b1991c0a730
Amine <ouas.amine@outlook.com>
2023-06-17 19:01:40 +0100

    Use formData()

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,13 +1,11 @@
 import SanityClient from "@/sanity/client"
 import { NextResponse } from "next/server"
-import xml2js from 'xml2js'
 
 export async function POST(request: Request) {
-  const response = await request.text()
-  const parser = new xml2js.Parser()
-  const test = await parser.parseStringPromise(response)
+  const response = await request.formData()
+  const status = response.pg_result
   await SanityClient.patch('00675708-865c-4187-88dd-de3bce751590').set({
-    title: `${test.pg_result}`,
+    title: `${status}`,
   }).commit()
   return NextResponse.json({ message: 'Hello world!' })
 }
