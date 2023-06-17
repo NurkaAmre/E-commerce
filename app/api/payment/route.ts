d4be1817ddc616eb5f78b0fb11df30f3d734ff94
Amine <ouas.amine@outlook.com>
2023-06-17 17:48:59 +0100

    Return mock response on the payment API endpoint

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,9 +1,11 @@
 import SanityClient from "@/sanity/client"
+import { NextResponse } from "next/server"
 
-export async function GET(request: Request) {
+export async function POST() {
   await SanityClient.patch('user.f96da416-a07b-457a-a3db-9d66c755c963').set({
-    phone: '12345',
+    phone: '123',
   }).commit()
   .then((res: any) => res)
   .catch((err: any) => err)
+  return NextResponse.json({ message: 'Hello world!' })
 }
