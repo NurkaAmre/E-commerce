aa6bee154296d767e7485c6594b4a8a298ecc6ab
Amine <ouas.amine@outlook.com>
2023-06-17 18:33:02 +0100

    Push request body

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
   await SanityClient.patch('00675708-865c-4187-88dd-de3bce751590').set({
-    title: `${request}`,
+    title: `${request.body}`,
   }).commit()
   .then((res: any) => res)
   .catch((err: any) => err)
