9e358398319cd388c99f63b97ee25066a64abd00
Amine <ouas.amine@outlook.com>
2023-06-17 19:03:26 +0100

    push response

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
   const response = await request.formData()
   const status = response.pg_result
   await SanityClient.patch('00675708-865c-4187-88dd-de3bce751590').set({
-    title: `${status}`,
+    title: `${response}`,
   }).commit()
   return NextResponse.json({ message: 'Hello world!' })
 }
