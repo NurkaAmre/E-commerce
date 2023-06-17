8c15b82ee67a7e4c8410a83cc50639478b17b21f
Amine <ouas.amine@outlook.com>
2023-06-17 18:48:20 +0100

    Passe XML

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,10 +1,13 @@
 import SanityClient from "@/sanity/client"
 import { NextResponse } from "next/server"
+import xml2js from 'xml2js'
 
 export async function POST(request: Request) {
   const response = await request.text()
+  const parser = new xml2js.Parser()
+  const { test } = await parser.parseStringPromise(response)
   await SanityClient.patch('00675708-865c-4187-88dd-de3bce751590').set({
-    title: `${response}`,
+    title: `${test.pg_result}`,
   }).commit()
   return NextResponse.json({ message: 'Hello world!' })
 }
