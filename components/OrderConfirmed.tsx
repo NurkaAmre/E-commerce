c3231fe061cb876f1ae1c09534a361055d398613
Amine <ouas.amine@outlook.com>
2023-06-18 14:47:32 +0100

    Create payment status report route

--- components/OrderConfirmed.tsx
+++ components/OrderConfirmed.tsx
@@ -12,22 +12,20 @@ export default function OrderConfirmed() {
     cartStore.clearCart()
   }, [])
 
-  const checkoutOrder = () => {
-    setTimeout(() => {
-      cartStore.setCheckout('cart')
-    }, 1000)
-    cartStore.toggleCart()
-  }
+  
     return(
-      <motion.div className='flex items-center justify-center my-12'
-      initial={{scale: 0.5, opacity: 0}} animate={{scale: 1, opacity: 1}}>
+      <motion.div 
+        className='flex items-center justify-center my-12'
+        initial={{scale: 0.5, opacity: 0}}
+        animate={{scale: 1, opacity: 1}}
+      >
         <div className='p-12 rounded-md text-center'>
           <h1 className='text-xl font-medium'>Your order has been placed </h1>
           <h2 className='text-sm my-4'>Check your email for the receipt</h2>
-          <Image src={dance} className='py-8' alt='dance'/>
+          <Image src={dance} width={300} height={300} className='py-8' alt='dance'/>
           <div className='flex items-center justify-center gap-12'>
-          <Link href={'/dashboard'}>
-            <button onClick={checkoutOrder} className='font-medium'>Check your Order</button>
+          <Link href={'/account/orders'}>
+            <button className='font-medium'>Check your Order</button>
           </Link>
         </div>
         </div>  
