825c3040059db93db4d684341c233a8c8d6fd496
Amine <ouas.amine@outlook.com>
2023-06-18 12:19:57 +0100

    Alter generateSig again to fix issues

--- util/generateSig.ts
+++ util/generateSig.ts
@@ -4,7 +4,8 @@ export default function generateSig(URL: string, dataObject: any) {
   const secretKey = 'xbiXRN7i69LFWK8x'
   let sig = Object.keys(dataObject)
     .sort()
-    .map((key) => key !== 'pg_sig' ? dataObject[key] : undefined)
+    .filter((key) => key !== 'pg_sig')
+    .map((key) => dataObject[key])
     .join(';')
   sig = md5(`${URL};${sig};${secretKey}`)
   return sig
