8359025849d079632dc6eba8681f5370e89f1b07
Amine <ouas.amine@outlook.com>
2023-06-19 20:38:18 +0100

    Replace daisyUI carousel with react-responsive-carousel

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,110 +1,109 @@
+'use client'
+
 import Image from "next/image";
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
-import SanityClient from "@/sanity/client";
 import discountPrice from "@/util/discountPrice";
 import AddCartButton from "@/components/AddCartButton";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import getProduct from "@/functions/getProduct";
+import { useEffect, useState } from "react";
+import LoadingAnimation from "@/components/loadingAnimation";
 
-export default async function ProductDetails({ params }: { params: { slug: string } }) {
+export default function ProductDetails({ params }: { params: { slug: string } }) {
   const slug = params.slug;
-  const query = `*[_type == "product" && slug.current == '${slug}'][0]{
-    "id": _id,
-    name,
-    price,
-    stock,
-    details,
-    type,
-    description,
-    discount,
-    "imagesURL": images[].asset->url
-  }`;
+  const [product, setProduct] = useState({} as ProductType);
+  useEffect(() => {
+    getProduct(slug).then((res) => {
+      setProduct(res.data as ProductType);
+    })
+  })
 
-  const product = await SanityClient.fetch(query);
-
-  return (
-    <div>
-      <div key={product._id} className="product-detail-container mt-[4rem]">
-        <div className="product-image md:w-1/2">
-          <div className="image-container">
-            <Image 
-              src={product.imagesURL[1]}
-              alt={product.name}
-              width={300}
-              height={300}
-              className="rounded-md img-product" />
-          </div>
-          <div className="small-images-container carousel w-full">
-            {product.imagesURL.map((imageURL: string, index: number) => {
-              if (index !== 0) {
-                return (
-                  <div id={`item${index}`} className="carousel-item w-full">
+  if (product.id) {
+    return (
+      <div>
+        <div className="product-detail-container mt-[4rem]">
+          <div className="product-image md:h-[300px] md:w-1/2">
+            <Carousel
+              renderThumbs={() => {
+                return product.imagesURL.map((imageURL: string) => {
+                  return (
                     <Image
+                      className="max-h-[100px]"
                       src={imageURL}
                       alt={product.name}
-                      width={150}
-                      height={150} />
-                  </div>
-                );
-              }
-            })}
-          </div>
-          <div className="flex justify-center w-full py-2 gap-2">
-          {product.imagesURL.map((imageURL: string, index: number) => {
-              if (index !== 0) {
+                      width={100}
+                      height={100} />
+                  )
+                })
+              }}
+            >
+              {product.imagesURL.map((imageURL: string) => {
                 return (
-                  <a href={`#item${index}`} className="justify-center btn btn-xs">{index}</a> 
-                );
-              }
-            })}
+                  <Image
+                    className="max-h-[400px]"
+                    src={imageURL}
+                    alt={product.name}
+                    width={400}
+                    height={400} />
+                )
+              })}
+            </Carousel>
           </div>
-        </div>
-
-        <div className="product-detail-desc ">
-          <div className="flex gap-20">
-            <h1 className="text-3xl md:text-6xl font-dancing_script text-gray-600">{product.name}</h1>
-            <div className="reviews">
-              <div className="review-stars">
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiOutlineStar />
+  
+          <div className="product-detail-desc">
+            <div className="flex gap-20">
+              <h1 className="text-3xl md:text-6xl font-dancing_script text-gray-600">{product.name}</h1>
+              <div className="reviews">
+                <div className="review-stars">
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiOutlineStar />
+                </div>
+                <p>
+                  (20)
+                </p>
               </div>
-              <p>
-                (20)
-              </p>
+            </div>
+            <div className="product-desc">
+              <h6 className="text-lg md:texl-xl font-castoro">{product.description}</h6>
+            </div>
+            <h3>Характеристики</h3>
+            <hr />
+            <p className="font-roboto">{product.details}</p>
+            <div className="add-cart flex gap-10 mt-5">
+              <div className="quantity">
+                <p className="quantity-desc">
+                  <span className="minus"><AiOutlineMinus /></span>
+                  <span className="num">0</span>
+                  <span className="plus" ><AiOutlinePlus /></span>
+                </p>
+              </div>
+              <div className="product-prices">
+                {(!product.discount) ? <p className="price">{product.price}&#x20B8;</p> : null}
+                {(product.discount) ? <p className="price">{discountPrice(product.price, product.discount)}&#x20B8;</p> : null}
+                {(product.discount) ? <p className="price-old">{product.price}&#x20B8;</p> : null}
+              </div>
+              <div className="discount">
+                <p className="pl-1">-{product.discount}%</p>
+              </div>
+            </div>
+            <div className="buttons">
+              <AddCartButton { ...product } />
+              <button type="button" className="buy-now">Купить</button>
             </div>
           </div>
-          <div className="product-desc">
-            <h6 className="text-lg md:texl-xl font-castoro">{product.description}</h6>
-          </div>
-          <h3>Характеристики</h3>
-          <hr />
-          <p className="font-roboto">{product.details}</p>
-          <div className="add-cart flex gap-10 mt-5">
-            <div className="quantity">
-              <p className="quantity-desc">
-                <span className="minus"><AiOutlineMinus /></span>
-                <span className="num">0</span>
-                <span className="plus" ><AiOutlinePlus /></span>
-              </p>
-            </div>
-            <div className="product-prices">
-              {(!product.discount) ? <p className="price">{product.price}&#x20B8;</p> : null}
-              {(product.discount) ? <p className="price">{discountPrice(product.price, product.discount)}&#x20B8;</p> : null}
-              {(product.discount) ? <p className="price-old">{product.price}&#x20B8;</p> : null}
-            </div>
-            <div className="discount">
-              <p className="pl-1">-{product.discount}%</p>
-            </div>
-          </div>
-          <div className="buttons">
-            <AddCartButton { ...product } />
-            <button type="button" className="buy-now">Купить</button>
-          </div>
+  
         </div>
-
+  
       </div>
-
-    </div>
-  );
+    );
+  } else {
+    return (
+      <LoadingAnimation />
+    )
+  }
+  
 }
