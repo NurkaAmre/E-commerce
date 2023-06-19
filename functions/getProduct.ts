2b231b43fb79b3002da1c41287486fd8cb7fc154
Amine <ouas.amine@outlook.com>
2023-06-19 20:37:37 +0100

    Create getProduct func to fetch product from db

---
+++ functions/getProduct.ts
@@ -0,0 +1,30 @@
+'use server'
+
+import SanityClient from "@/sanity/client";
+
+export default async function getProduct(productSlug: string) {
+  let status = {code: 0, message: '', data: {}};
+  const query = `*[_type == "product" && slug.current == $productSlug][0]{
+    "id": _id,
+    name,
+    price,
+    stock,
+    details,
+    type,
+    description,
+    discount,
+    "imagesURL": images[].asset->url
+  }`;
+
+  const product = await SanityClient.fetch(query, {productSlug});
+
+  if (product) {
+    status.code = 200;
+    status.message = 'Product fetched successfully';
+    status.data = product;
+  } else {
+    status.code = 500;
+    status.message = 'Something went wrong, could not reach the product details';
+  }
+  return status;
+} 
