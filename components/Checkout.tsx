0fc4899dbf2058d2d232c2db0b84cfdbc999dbef
NurkaAmre <nurkerey30@gmail.com>
2023-06-20 15:35:16 +0600

    Add checkout form

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -94,61 +94,96 @@ export default function Checkout({ user }: { user: UserType }) {
         setMessage(updateDataResponse.message)
       }
     } else {
-      setMessage('Please fill in all the fields')
+      setMessage('Пожалуйста, заполните все поля.')
     }
   }
 
   return (
     <div>
       <div>
-        <h2>Order Products</h2>
         <div>
           {cartStore.cart.map((item) => (
             <motion.div layout key={item.id}
-              className='flex p-4 gap-4 bg-base-100 my-4 rounded-lg'>
+              className='flex p-4 gap-4 bg-base-100 my-2 rounded-lg'>
               <Image className='rounded-md h-24' src={item.imagesURL[0]} alt={item.name} width={120} height={120}/>
               <div>
-                <h2>{item.name}</h2>
+                <h2 className='font-dancing_script'>{item.name}</h2>
                 <div className='flex gap-2'>
-                  <h2>Количество: </h2>
+                  <h2 className='font-lobster'>Количество:{item.quantity} </h2>
                   <h2>{item.quantity}</h2>
                 </div>
-                <p className='text-sm'>{discountPrice(item.price, item.discount)} KZT</p>
+                <p className=''>{discountPrice(item.price, item.discount)} <span className='text-teal-400'>KZT</span></p>
               </div>
             </motion.div>
           ))}
-          <p className=''>Сумма к оплате: {totalPrice} KZT</p>
+          <p className='font-bold text-gray-600 mt-6'>Сумма к оплате: {totalPrice}<span className='text-teal-400'>KZT</span></p>
         </div>
       </div>
 
       {/* Shipping Information */}
-      <h2 className='my-4'>Shipping Information</h2>
+      <h2 className='my-4 font-lobster text-2xl mb-4 text-gray-400'>Информация о доставке</h2>
+      <hr />
       <div className='flex flex-col gap-4 justify-center relative'>
         <AiFillEdit className='absolute cursor-pointer text-[#8CCCC1]  right-0 top-0 text-2xl' onClick={() => setEditMode(true)} />
-        <div className='flex flex-col text-gray-700'>
-          <label className='text-sm'>Имя<span className='text-red-600'>*</span> </label>
-          {!editMode ? <span className='user-input text-base font-roboto'>{name}</span>
-            : <input className='user-input text-base font-roboto' type="text" value={name} onChange={handleNameChange} />}
+        <div className='flex flex-col text-gray-500'>
+          <label className='head-little'>Имя<span className='text-red-600'>*</span> </label>
+          {!editMode ? <span className='user-input text-xs font-roboto'>{name}</span>
+            : <input className='user-input text-xs font-roboto' type="text" value={name} onChange={handleNameChange} />}
         </div>
-        <div className='flex flex-col text-gray-700'>
-          <label className='text-sm'>Тел<span className='text-red-600'>*</span> </label>
-          {!editMode ? <span className='user-input text-base font-roboto'>{user.phone}</span>
-            : <input className='user-input text-base font-roboto' type="tel" value={phone} onChange={handlePhoneChange} />}
+        <div className='flex flex-col text-gray-500'>
+          <label className='head-little'>Тел<span className='text-red-600'>*</span> </label>
+          {!editMode ? <span className='user-input text-xs font-roboto'>{user.phone}</span>
+            : <input className='user-input text-xs font-roboto' type="tel" value={phone} onChange={handlePhoneChange} />}
         </div>
-        <div className='flex flex-col'>
-          <label className='text-sm'>Адрес доставки<span className='text-red-600'>*</span> </label>
-          {!editMode ? <span className='user-input text-base font-roboto'>{user.address?.street}</span>
-            : <input className='user-input text-base font-roboto' type="text" value={street} onChange={handleStreetChange} />}
+        <div className='flex flex-col text-gray-500'>
+          <label className='head-little'>Адрес доставки<span className='text-red-600'>*</span> </label>
+          {!editMode ? <span className='user-input text-xs font-roboto'>{user.address?.street}</span>
+            : <input className='user-input text-xs font-roboto' type="text" value={street} onChange={handleStreetChange} />}
         </div>
-        <div className='flex flex-col text-gray-700'>
-          <label className='text-sm'>Город<span className='text-red-600'>*</span> </label>
-          {!editMode ? <span className='user-input text-base font-roboto'>{user.address?.city}</span>
-            : <input className='user-input text-base font-roboto' type="text" value={city} onChange={handleCityChange} />}
+        <div className='flex flex-col text-gray-500'>
+          <label className='head-little'>Город<span className='text-red-600'>*</span> </label>
+          {!editMode ? <span className='user-input text-xs font-roboto'>{user.address?.city}</span>
+            : <input className='user-input text-xs font-roboto' type="text" value={city} onChange={handleCityChange} />}
         </div>
-        <div className='flex flex-col'>
-          <label className='text-sm'>Индекс<span className='text-red-600'>*</span> </label>
-          {!editMode ? <span className='user-input text-base font-roboto'>{user.address?.zip}</span>
-            : <input className='user-input text-base font-roboto' type="text" value={zip} onChange={handleZipChange} />}
+        <div className='flex flex-col text-gray-500'>
+          <label className='head-little'>Индекс<span className='text-red-600'>*</span> </label>
+          {!editMode ? <span className='user-input text-xs font-roboto'>{user.address?.zip}</span>
+            : <input className='user-input text-xs font-roboto' type="text" value={zip} onChange={handleZipChange} />}
+        </div>
+        <div>
+          <label className='text-base text-gray-700'>
+            Срок доставки
+            <span className='text-red-600'>*</span>
+            <input
+              type="date"
+              name="deliveryDate"
+              className='mx-6 text-teal-400 rounded-lg py-2 px-3'
+            />
+          </label>
+        </div>
+
+        <div className='flex justify-around'>
+          <div>
+            <label className='text-base text-gray-700'>
+              Вне города
+              <input
+                type="checkbox"
+                name="outsideCity"
+                className='mx-2 text-teal-300'
+              />
+            </label>
+          </div>
+
+          <div>
+            <label className='text-base text-gray-700'>
+              Требуется сборка
+              <input
+                type="checkbox"
+                name="requireConstruction"
+                className='mx-2 text-teal-300 rounded-full'
+              />
+            </label>
+          </div> 
         </div>
       </div>
 
@@ -159,13 +194,13 @@ export default function Checkout({ user }: { user: UserType }) {
             onClick={completeOrder} 
             className='btn mt-[2rem] w-full md:w-1/2 rounded-full'
           >
-            Confirm Order
+            Оформить Заказ
           </button>
         )}
 
         {isLoading && (
           <button className='btn mt-[2rem] w-full md:w-1/2 rounded-full bg-gray-100' disabled={true}>
-            Processing the order...
+            Обработка заказа...
           </button>
         )}
       </div>
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -73,12 +73,12 @@ export default function UserInfo({ user }: any) {
           <h3 className='text-2xl my-4 font-castoro font-bold text-gray-600'>Контакты</h3>
           <AiFillEdit className='absolute cursor-pointer text-[#8CCCC1]  right-0 top-5 text-2xl' onClick={() => setContactInfoEditMode(true)} />
           <div className='flex flex-col my-3  text-gray-700'>
-            <label className='text-sm'>Тел<span className='text-red-600'>*</span> </label>
+            <label className='head-little'>Тел<span className='text-red-600'>*</span> </label>
             {!contactInfoEditMode ? <span className='user-input text-[10px] font-roboto'>{user.phone}</span>
               : <input className='user-input text-[10px] font-roboto' type="tel" value={phone} onChange={handlePhoneChange} />}
           </div>
           <div className='flex flex-col my-3'>
-            <label className='text-sm'>E-mail<span className='text-red-600'>*</span> </label>
+            <label className='head-little'>E-mail<span className='text-red-600'>*</span> </label>
             {!contactInfoEditMode ? <span className='user-input text-[10px] font-roboto'>{user.email}</span>
               : <input className='user-input text-[10px] font-roboto' type="email" value={email} onChange={handleEmailChange} />}
           </div>
@@ -89,17 +89,17 @@ export default function UserInfo({ user }: any) {
           <h2 className='text-2xl my-4 font-castoro font-bold text-gray-600 '>Адрес</h2>
           <AiFillEdit className='absolute cursor-pointer text-[#8CCCC1]  right-0 top-5 text-2xl' onClick={() => setAddressEditMode(true)} />
           <div className='flex flex-col my-3'>
-            <label className='text-sm'>Адрес доставки<span className='text-red-600'>*</span> </label>
+            <label className='head-little'>Адрес доставки<span className='text-red-600'>*</span> </label>
             {!addressEditMode ? <span className='user-input text-[10px] font-roboto'>{user.address?.street}</span>
               : <input className='user-input text-[10px] font-roboto' type="text" value={street} onChange={handleStreetChange} />}
           </div>
           <div className='flex flex-col my-3 text-gray-700'>
-            <label className='text-sm'>Город<span className='text-red-600'>*</span> </label>
+            <label className='head-little'>Город<span className='text-red-600'>*</span> </label>
             {!addressEditMode ? <span className='user-input text-[10px] font-roboto'>{user.address?.city}</span>
               : <input className='user-input text-[10px] font-roboto' type="text" value={city} onChange={handleCityChange} />}
           </div>
           <div className='flex flex-col text-[10px] my-3'>
-            <label className='text-sm'>Индекс<span className='text-red-600'>*</span> </label>
+            <label className='head-little'>Индекс<span className='text-red-600'>*</span> </label>
             {!addressEditMode ? <span className='user-input text-[10px] font-roboto'>{user.address?.zip}</span>
               : <input className='user-input text-[10px] font-roboto' type="text" value={zip} onChange={handleZipChange} />}
           </div>
