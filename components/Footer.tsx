b1c4a2a42532e262f2b7db1696182fdc0e160e86
NurkaAmre <nurkerey30@gmail.com>
2023-07-02 22:17:42 +0600

    Removed discount section below

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -70,16 +70,16 @@ function Footer() {
         </div>
       </div>
 
-      <div className='max-w-fit mx-auto py-4 font-lobster font-bold'>
-        <span>Made with <i className='text-red-700 text-xl'>&#9829; </i> by 
+      {/* <div className='max-w-fit mx-auto py-4 font-lobster font-bold'>
+        <span>Made with <i className='text-red-700 text-xl'>&#9829; </i> by
           <Link className='text-blue-400 underline mx-1' target="_blank"
-            rel="noreferrer" href={'https://github.com/NurkaAmre'}>Nurka</Link> 
-          & 
+            rel="noreferrer" href={'https://github.com/NurkaAmre'}>Nurka</Link>
+          &
           <Link className='text-blue-400 underline mx-1' target="_blank"
             rel="noreferrer" href={'https://github.com/ouasamine'}>Amine</Link>
 
         </span>
-      </div>
+      </div> */}
     </footer>
   );
 }
--- components/main.tsx
+++ components/main.tsx
@@ -5,7 +5,7 @@ import sofa from '@/public/set1.png'
 export default function MainSection (){
   return (
     <>
-      <div className="grid grid-cols-1 w-4/5 mx-auto font-[cormorant] mt-[180px] md:grid-cols-2">
+      <div className="grid md:mb-[10rem] grid-cols-1 w-4/5 mx-auto font-[cormorant] mt-[180px] md:grid-cols-2">
         <div className="flex flex-col items-center text-center pb-4 md:py-12 bg-[#788A74]">
           <div className='heading mt-8'>
             <h2 className='text-4xl md:text-6xl text-white leading-9'><span className='highLight text-5xl md:text-7xl'>М</span>ебельный магазин</h2>
