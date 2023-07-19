edceb7f20613319995337d563352db3a0e7d2b29
NurkaAmre <nurkerey30@gmail.com>
2023-07-19 15:17:27 +0600

    Fix picture size on mobile version

--- components/Product.tsx
+++ components/Product.tsx
@@ -5,13 +5,13 @@ import LikeButton from "@/components/LikeButton";
 export default function Product({ product }: { product: ProductType }) {
   return (
     <Link href={`/product/${product.slug.current}`} key={product.id}>
-      <div className="flex flex-col px-4 h-[270px] w-[280px] md:w-[375px] md:h-[375px] rounded-lg justify-center items-center -inset-2 shadow-2xl">
+      <div className="flex flex-col mt-4 px-4 h-[270px] w-[280px] md:w-[375px] md:h-[375px] rounded-lg justify-center items-center -inset-2 shadow-2xl">
         <Image
           src={product.imagesURL[0]}
           alt={product.name}
           width={400}
           height={300}
-          className="max-w-[170px] max-h-[130px] md:max-h-[230px] h-auto transform hover:scale-110 transition-transform duration-300 md:max-w-[300px]"
+          className="max-w-[250px] object-fill max-h-[130px] md:max-h-[230px] h-auto transform hover:scale-110 transition-transform duration-300 md:max-w-[300px]"
         />
         <div className="flex justify-center gap-5 items-center my-4 w-full max-w-[400px]">
           <div className="flex text-lg gap-2  text-gray-600 font-[dancingScript]">
