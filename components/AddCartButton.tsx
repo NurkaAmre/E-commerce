fce363c27f265e42ba0f5c3c2651d0c8d6892559
NurkaAmre <nurkerey30@gmail.com>
2023-07-19 15:32:55 +0600

    Fix buttons size

--- components/AddCartButton.tsx
+++ components/AddCartButton.tsx
@@ -41,7 +41,7 @@ const AddCartButton = (
     <button
     onClick={handleAddToCart} 
     disabled={added}
-    className="bg-[#8CCCC1] text-white rounded-3xl cursor-pointer text-lg font-medium whitespace-nowrap hover:scale-110 transition-transform duration-500 ease-out px-4 py-2 min-w-[180px]">
+        className="bg-[#8CCCC1] text-white rounded-3xl cursor-pointer text-lg font-medium whitespace-nowrap hover:scale-110 transition-transform duration-500 ease-out px-4 py-2 min-w-full md:min-w-[180px]">
       {!added && <span>В корзину</span>}
         {added && <span>Добавить</span>}
     </button>
