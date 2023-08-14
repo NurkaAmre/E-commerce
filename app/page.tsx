74a19b713ac73c013afce446c350581e9a87bf6a
NurkaAmre <nurkerey30@gmail.com>
2023-08-14 22:41:44 +0600

    Add google mambo jambo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Discount from "../components/discount"
 import MainSection from "../components/main"
 import ProductSection from "../components/product-section"
@@ -6,6 +7,19 @@ import DeliveryServices from "../components/DeliveryServices"
 export default function Main (){
   return (
     <>
+      <Head>
+        {/* Google Analytics tracking code */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W76140GXRJ"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-W76140GXRJ');
+          `}
+        </script>
+      </Head>
       <MainSection />
       <ProductSection />
       <Discount />
